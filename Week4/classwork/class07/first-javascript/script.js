8be3d19242735061f4daddd4aeeb43e9e409ed12
kateladenheim <ladenheim.k@gmail.com>
2016-06-27 21:04:38 -0400

    class7

---
+++ Week4/classwork/class07/first-javascript/script.js
@@ -0,0 +1,24 @@
+var firstName = "Kate";
+var lastName = "Ladenheim";
+var num1 =10*10;
+var num2=100/10;
+var num3=50-17;
+var num4=50+17;
+console.log("hello!")
+
+document.write(num1 + '<br/>');
+document.write(num2 + '<br/>');
+document.write(num3 + '<br/>');
+document.write(num4 + '<br/>');
+document.write(firstName + " " + lastName);
+
+//function definition
+
+function doSomething(){
+	alert('this is an alert');
+}
+
+doSomething()
+
+document.getElementById('greeting').style.color = "red";
+document.getElementById('myPara').style.color = "green";
