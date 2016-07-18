16690289512f6b40a78f7490717fbb78e7575638
kateladenheim <ladenheim.k@gmail.com>
2016-07-18 19:30:16 -0400

    commit

--- week6/homework/temperature-converter/js/project.js
+++ week6/homework/temperature-converter/js/project.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
 		temp = parseInt($('#temperature').val());
 		$('#solution').html((temp - 32) / 1.8);
 
-		var solution = parseInt($('#solution').val());
+		var solution = $('#solution').val();
 
-		if (solution < 20) {
+		if (solution <= 20) {
 			$('#body').css('background-color', '#75b7bb');
 		} else { 
 			$('#body').css('background-color', '#ba0913');
@@ -21,9 +21,9 @@ $(document).ready(function () {
 		temp = parseInt($('#temperature').val());
 		$('#solution').html(temp * 1.8 + 32);
 
-		var solution = parseInt($('#solution').val());
+		var solution = $('#solution').val();
 
-		if (solution < 70) {
+		if (solution <= 70) {
 			$('#body').css('background-color', '#75b7bb');
 		} else { 
 			$('#body').css('background-color', '#ba0913');
