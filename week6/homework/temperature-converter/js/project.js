e268f529a5971286803e29517035091465b763cf
kateladenheim <ladenheim.k@gmail.com>
2016-07-18 19:00:24 -0400

    updates

--- week6/homework/temperature-converter/js/project.js
+++ week6/homework/temperature-converter/js/project.js
@@ -1,27 +1,33 @@
 $(document).ready(function () {
 
-	var temp = $('#temperature').val();
-	var solution = $('#solution').val();
+	var temp;
+	var solution;
 
 	$('#celcius').click(function(){
-		$('#soluion').text((temp - 32) / 1.8);
+		temp = parseInt($('#temperature').val());
+		$('#solution').html((temp - 32) / 1.8);
+
+		var solution = parseInt($('#solution').val());
 
 		if (solution < 20) {
 			$('#body').css('background-color', '#75b7bb');
-	} else { 
-		$('#body').css('background-color', '#ba0913');
-	}
+		} else { 
+			$('#body').css('background-color', '#ba0913');
+		}
 
 	});
 
 	$('#farenheit').click(function() {
-		$('#solution').text(temp * 1.8 + 32);
+		temp = parseInt($('#temperature').val());
+		$('#solution').html(temp * 1.8 + 32);
+
+		var solution = parseInt($('#solution').val());
 
 		if (solution < 70) {
 			$('#body').css('background-color', '#75b7bb');
-	} else { 
-		$('#body').css('background-color', '#ba0913');
-	}
+		} else { 
+			$('#body').css('background-color', '#ba0913');
+		}
 
 	});
 
