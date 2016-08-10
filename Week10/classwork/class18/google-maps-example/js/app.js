9f1b7f08ec902b23a97188f89bfc9c3cada055cc
kateladenheim <ladenheim.k@gmail.com>
2016-08-10 18:27:14 -0400

    commit

--- Week10/classwork/class18/google-maps-example/js/app.js
+++ Week10/classwork/class18/google-maps-example/js/app.js
@@ -1,5 +1,17 @@
+	var map;
+      function initMap() {
+	        map = new google.maps.Map(document.getElementById('map'), {
+	          center: {lat: 40.7127, lng: -74.0059},
+	          zoom: 8
+        	});
+
+      var marker = new google.maps.Marker({
+			position: {lat: 40.7127, lng: -74.0059},
+			map: map,
+			title: 'New York, NY'
+			});
+    	}
+
 $(document).ready(function() {
 
 })
-
-
